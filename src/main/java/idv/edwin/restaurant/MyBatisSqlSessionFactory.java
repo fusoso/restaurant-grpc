7ad0b89869461a69b364f420efdc705cc1eb8004
edwin_wang <edwin_wang@gss.com.tw>
2021-12-15 02:03:42 +0800

    remove unnecessary code.

--- src/main/java/idv/edwin/restaurant/MyBatisSqlSessionFactory.java
+++ src/main/java/idv/edwin/restaurant/MyBatisSqlSessionFactory.java
@@ -6,24 +6,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class MyBatisSqlSessionFactory {
-	private final static String USERNAME = "${username}";
-	private final static String PASSWORD = "${password}";
 
 	public static SqlSessionFactory buildSqlSessionFactory() throws ClassNotFoundException {
-//        DataSource dataSource
-//                = new PooledDataSource("com.mysql.cj.jdbc.Driver",
-//                "jdbc:mysql://localhost:3306/${db_name}?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull",
-//                USERNAME,
-//                PASSWORD);
-//        Environment environment
-//                = new Environment("Development", new JdbcTransactionFactory(), dataSource);
-//
-//        Configuration configuration = new Configuration(environment);
-//        configuration.addMapper(FoodMapper.class);
-//
-//
-//        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-//        return builder.build(configuration);
 		InputStream inputStream = null;
 		try {
 			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
